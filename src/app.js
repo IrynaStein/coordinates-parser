b65813d7ced1081e90301ef1217ae68784878686
Iryna Stein <isophiestein@gmail.com>
2021-10-28 14:33:29 -0600

    express set up in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("hello")
+// const path = require('path')
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Iryna Stein - test ')
+  })
+ 
+  
+
+
+
+  app.listen(3000)
